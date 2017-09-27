170ad321b228a09f469c4d04103ca12ff9058077
narosen <1311naro@gmail.com>
2017-09-28 01:39:28 +0530

    Implimenting jump and changing the values of y,z ans angle x of CameraController
    I have included jump of the egg in PlayerController script and changed
    the cameraController variable values a bit to make it look more top down
    as it was hard to see what was behind the egg. Modify the code if there
    are better solutions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+	public float jumpForce;
 
-    private Rigidbody rb;
+	private bool grounded;	//checking if the egg is grounded
+	private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+	}
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+		//For jumping
+		if (Input.GetButtonDown ("Jump") && grounded == true) {
+			movement.y = jumpForce;				
+		}
+        
         rb.AddForce(movement * speed);
     }
+
+
+	//checking if the egg is on the Floor or not
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "Floor") {
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if (col.collider.tag == "Floor") {
+			grounded = false;
+		}
+	}
+
 }
