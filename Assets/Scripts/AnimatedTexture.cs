e1ecbd83c61e4fe5c3be6e360cc625cabad13fd2
Hadi <abdulhadiyq@gmail.com>
2017-09-28 01:09:28 +0300

    Animated Texture Class. Added test blinking texture.

---
+++ Assets/Scripts/AnimatedTexture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTexture : MonoBehaviour {
+
+    [System.Serializable]
+    public class AnimatedTextureFrame
+    {
+        public Texture texture;
+        public float duation;
+    }
+    public List<AnimatedTextureFrame> frames = new List<AnimatedTextureFrame>();
+    int currentFrame = 0;
+    float time = 0f;
+    Material material;
+
+	// Use this for initialization
+	void Start () {
+        if (frames.Count > 0)
+        {
+            material = GetComponent<MeshRenderer>().material;
+            material.SetTexture("_MainTex", frames[0].texture);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (frames.Count>0 && time>frames[currentFrame].duation)
+        {
+            time = 0;
+            Debug.Log(currentFrame + " : " + (currentFrame + 1) % frames.Count);
+            currentFrame = (currentFrame + 1) % frames.Count;
+            material.SetTexture("_MainTex", frames[currentFrame].texture);
+            Debug.Log(frames[currentFrame].texture.name);
+        }
+	}
+}
