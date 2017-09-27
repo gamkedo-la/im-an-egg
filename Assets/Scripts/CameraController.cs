1baade282bd920a71de38f30452257aeab6e50ff
Kornel <Kornel@Kornel.com>
2017-09-27 10:23:33 +0200

    Added camera controller which follows the player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+	[Header("Dependencies")]
+	[SerializeField, Tooltip("The player object.")] private GameObject player;
+
+	[Header("Parameters")]
+	[SerializeField, Tooltip("How smooth the camera movement will be (lower = smoother).")] private float smoothing = 0.5f;
+	[SerializeField, Tooltip("Position relative to the player.")] private Vector3 offset = new Vector3(0, 2.5f, -9.5f);
+	[SerializeField, Tooltip("Camera tilt in the X axis.")] private float angleX = 15.0f;
+
+	private Vector3 positionVelocity;
+
+	void Start( )
+	{
+		Assert.IsNotNull( player );
+
+		transform.Rotate( Vector3.right, angleX );
+	}
+
+	void FixedUpdate( )
+	{
+		Vector3 newPosition = player.transform.position + offset;
+		transform.position = Vector3.SmoothDamp( transform.position, newPosition, ref positionVelocity, smoothing );
+	}
+}
