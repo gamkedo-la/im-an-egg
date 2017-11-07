00cc5ff3c2cdc6468fd26ea5e869e917a7cd2d54
Kornel <Kornel@Kornel.com>
2017-11-07 11:14:09 +0100

    Added more egs to the selection screen and added more egg type files.

---
+++ Assets/Scripts/SelectionScreenSetter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionScreenSetter : MonoBehaviour
+{
+	public EggType Type;
+	public Button Button;
+
+	private AnimatedTexture tex;
+
+	void Start ()
+	{
+		tex = GetComponent<AnimatedTexture>( );
+
+		if (Type != null && Button != null)
+		{
+			tex.frames = Type.frames;
+			Button.GetComponentInChildren<Text>( ).text = Type.Name;
+		}
+	}
+}
