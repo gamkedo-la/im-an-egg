a11a26ec26592151c87aae243c9499fd4eaa00ca
Jeff Penny <jeffpennyaudio@gmail.com>
2017-11-17 03:49:33 -0500

    Attempt to add audio on collision but need help

---
+++ Assets/Scripts/AudioEggShell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioEggShell : MonoBehaviour {
+
+	public AudioClip[] eggShell;
+	public AudioMixerGroup output;
+	public float minPitch = .85f;
+	public float maxPitch = 1.15f;
+	public float minVol = .85f;
+	public float maxVol = 1.15f;
+
+	void OnCollisionEnter()
+	{
+		PlayShellPiece();
+	}
+
+	void PlayShellPiece()
+	{
+		int randomClip = Random.Range (0, eggShell.Length);
+		AudioSource shellSource = gameObject.AddComponent<AudioSource>();
+		shellSource.clip = eggShell [randomClip];
+		shellSource.outputAudioMixerGroup = output;
+		shellSource.pitch = Random.Range (minPitch, maxPitch);
+		shellSource.volume = Random.Range (minVol, maxVol);
+		shellSource.Play ();
+		Destroy (shellSource, eggShell[randomClip].length);
+	}
+}
