a9672f4c30c53701df2b250381c41ed49a708f5e
Erik Verlage <everlage88@gmail.com>
2017-11-26 15:22:57 -0500

    Main Menu / Credits added

---
+++ Assets/Scripts/GateTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GateTrigger : MonoBehaviour {
+
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Object Entered Trigger");
+
+		SceneManager.LoadScene("halfpipe");
+	}
+	
+	
+
+}
