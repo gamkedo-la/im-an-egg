c0bd8767d04efda6b7da035d96ade8f57fc21d8d
Erik Verlage <everlage88@gmail.com>
2017-12-03 14:29:29 -0500

    Added 2 levels

---
+++ Assets/Scripts/GateTriggerToCredits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTriggerToCredits : MonoBehaviour {
+	public Transform spawnTransform;
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Object Entered Trigger");
+
+		other.transform.parent.transform.position = spawnTransform.position;
+	}
+}
